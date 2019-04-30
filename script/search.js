394538bfe182b27199f10acc6b5362668ca08481
sebastienfernandez <sebastien.fdu78@gmail.com>
2019-04-30 11:44:44 +0200

    integration de l'image de couverture à la listes des musiques et la touche enter déclenche la validation du formulaire

--- script/search.js
+++ script/search.js
@@ -3,9 +3,17 @@ $(function() {
     // champ titre focus d'entrée de jeu
 
     $("#title").focus();
-    
 
-    //liste des titres eminem lors du clic
+
+    //l'appui de la touche enter déclenche la validation du formulaire
+
+    $(document).on('keypress',function(e) {
+        if(e.which == 13) {
+            $("#button-submit").click();
+        }
+    });
+
+    //liste des titres de musique selon l'artiste lors du clic
 
     $("#button-submit").click(function() {
         console.log("j'ai cliqué!");
@@ -17,10 +25,12 @@ $(function() {
             console.log(musiques);
         
             document.querySelector('#results').innerHTML =
-                    musiques.data.map(m => '<h3>' + m.title + '</h3>' 
+                    musiques.data.map(m => '<div class="results-box"><h3>' + m.title + '</h3>' 
+                    + '<img src=' + m.album.cover_medium + ' alt="logo couverture" class="music-cover"/>'
                     + '<span class="artist-name">' + m.artist.name +'</span>'
                     + '<p>' + m.album.title + '</p>'
-                    + '<audio src=' + m.preview + ' controls></audio>')
+                    + '<audio src=' + m.preview + ' controls>Veuillez mettre à jour votre navigateur ! </audio>'
+                    + '<button class="favorite-button">Bouton des favoris</button></div>')
                     .join('<br>');
         
         });
