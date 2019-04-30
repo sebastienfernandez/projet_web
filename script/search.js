afd46019db785a89b234d69be287ab000ddac339
sebastienfernandez <sebastien.fdu78@gmail.com>
2019-04-30 09:52:41 +0200

    le bouton submit du formaulaire renvoie tous les titres de emminem et le champ du titre est en focus

--- script/search.js
+++ script/search.js
@@ -1,13 +1,18 @@
 $(function() {
-    $.ajax({
-        url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
-        dataType : 'jsonp'
-    }).done(function(musiques) {
-    
-        console.log(musiques);
-    
-        document.querySelector('#results').innerHTML =
-                musiques.data.map(m => m.title).join('<br>');
-    
+    $("#title").focus();
+    $("#button-submit").click(function() {
+        console.log("j'ai cliqué!");
+        $.ajax({
+            url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
+            dataType : 'jsonp'
+        }).done(function(musiques) {
+        
+            console.log(musiques);
+        
+            document.querySelector('#results').innerHTML =
+                    musiques.data.map(m => m.title).join('<br>');
+        
+        });
     });
+    
 });
