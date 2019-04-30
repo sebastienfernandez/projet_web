893eaffb7f708f6a3c64137fd13070ea99f68684
sebastienfernandez <sebastien.fdu78@gmail.com>
2019-04-30 10:54:55 +0200

    ajout d'une nouvelle police et requête qui affiche le nom de musique, le nom de l'artiste et de l'album

--- script/search.js
+++ script/search.js
@@ -3,20 +3,24 @@ $(function() {
     // champ titre focus d'entrée de jeu
 
     $("#title").focus();
+    
 
     //liste des titres eminem lors du clic
 
     $("#button-submit").click(function() {
         console.log("j'ai cliqué!");
         $.ajax({
-            url : 'https://api.deezer.com/search?q=eminem&output=jsonp',
+            url : 'https://api.deezer.com/search?q=' + $("#title").val() + '&output=jsonp',
             dataType : 'jsonp'
         }).done(function(musiques) {
         
             console.log(musiques);
         
             document.querySelector('#results').innerHTML =
-                    musiques.data.map(m => m.title).join('<br>');
+                    musiques.data.map(m => '<h3>' + m.title + '</h3>' 
+                    + '<span class="artist-name">' + m.artist.name +'</span>'
+                    + '<p>' + m.album.title + '</p>')
+                    .join('<br>');
         
         });
     });
